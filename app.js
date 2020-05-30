b500cd7740c9d9d7a009f415cde60a07da7acafe
Brian Harney <brian.r.harney@gmail.com>
2020-05-30 11:57:51 -0500

    added data-keys for js use

---
+++ app.js
@@ -0,0 +1,61 @@
+const calculator = document.querySelector('#calculator');
+const keys = calculator.querySelector('#button-container');
+const display = document.querySelector('#display');
+const previousKeyType = calculator.dataset.previousKeyType;
+
+
+const calculate = (n1, operator, n2) => {
+    let result = '';
+
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2);
+    }
+    return result;
+}
+
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'divide' ||
+            action === 'multiply'
+        ) {
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+        }
+
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            display.textContent = calculate(firstValue, operator, secondValue);
+        }
+    }
+})
